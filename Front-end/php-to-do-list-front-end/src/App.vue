ba6d5ff82ed28c28637392bd57739eb6b91c92ad
Jac22 <jacopo.prudentino@gmail.com>
2023-01-13 13:05:42 +0100

    recuperate e scritte sulla pagina le tasks

--- Front-end/php-to-do-list-front-end/src/App.vue
+++ Front-end/php-to-do-list-front-end/src/App.vue
@@ -1,13 +1,35 @@
 <script>
+import axios from 'axios';
+const APIServer = 'http://localhost/';
 export default {
+  data() {
+    return {
+      toDoList: [],
+    }
+  },
+  methods: {
+    getData: function () {
+      axios.get(APIServer + 'sendDataApi.php')
+        .then((res) => { this.toDoList = res.data }
+        );
+    }
+  },
 
+  mounted() {
+    this.getData();
+  }
 }
 </script>
 
 <template>
-
+  <h1>To Do List</h1>
+  <ul>
+    <li v-for="task in this.toDoList" :class="{ 'done': task.done }">{{ task.text }}</li>
+  </ul>
 </template>
 
 <style>
-
+.done {
+  text-decoration: line-through;
+}
 </style>
